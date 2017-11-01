79dec75bfa937799ab78c29305d42f6311a122c0
Adel Shah <shadel.shsh@gmail.com>
2017-11-01 19:02:44 -0400

    init

--- models/majors.js
+++
@@ -1,8 +0,0 @@
-const dbs = require('../util/dbconnection')
-
-exports.getAll = function(done){
-	dbs.get().query(`SELECT * FROM majors_table`, (err,rows)=>{
-		if(err) return done(err)
-		done(null,rows)
-	})
-}
