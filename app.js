5697fd1c6d709be6bbf33554e9cd4adaa8eb21c9
Adel Shah <shadel.shsh@gmail.com>
2017-11-02 01:45:53 -0400

    some stuff added

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
+var bodyParser = require('body-parser'),
+ 	swig = require('swig');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var student = require('./routes/student');
